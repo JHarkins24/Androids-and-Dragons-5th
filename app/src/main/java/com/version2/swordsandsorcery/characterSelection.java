bb6885612e138c302779d6c95976f2e44b11cfd2
Sparky <spenciev3@yahoo.com>
2019-03-05 21:46:52 -0800

    Got basic layout of character selection

---
+++ app/src/main/java/com/version2/swordsandsorcery/characterSelection.java
@@ -0,0 +1,13 @@
+package com.version2.swordsandsorcery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class characterSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_character_selection);
+    }
+}
