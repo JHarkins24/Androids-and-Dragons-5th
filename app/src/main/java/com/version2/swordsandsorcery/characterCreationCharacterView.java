d219d78b90b2016fc10703727005f6cab7809571
Sparky <spenciev3@yahoo.com>
2019-03-13 14:02:16 -0700

    almost done with pdf thing

--- app/src/main/java/com/version2/swordsandsorcery/characterCreationCharacterView.java
+++ app/src/main/java/com/version2/swordsandsorcery/characterCreationCharacterView.java
@@ -1,10 +1,15 @@
 package com.version2.swordsandsorcery;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.List;
 
 public class characterCreationCharacterView extends AppCompatActivity {
 
@@ -76,5 +81,23 @@ public class characterCreationCharacterView extends AppCompatActivity {
                 startActivity(new Intent(characterCreationCharacterView.this,main_menu.class));
             }
         });
+
+        final Button Save = findViewById(R.id.button17);
+        Save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("../pdf.pdf"));
+                intent.setType("application/pdf");
+                PackageManager pm = getPackageManager();
+                List<ResolveInfo> activities = pm.queryIntentActivities(intent, 0);
+                if (activities.size() > 0) {
+                    startActivity(intent);
+                } else {
+                    startActivity(new Intent(characterCreationCharacterView.this,main_menu.class));
+                }
+            }
+        });
+
     }
 }
