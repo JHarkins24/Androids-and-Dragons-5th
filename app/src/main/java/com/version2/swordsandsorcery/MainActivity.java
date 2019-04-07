c23008d05be5b6ed76e3279db1e76c20b48b00b9
Sparky <spenciev3@yahoo.com>
2019-04-06 19:23:45 -0700

    cleaned up imports

--- app/src/main/java/com/version2/swordsandsorcery/MainActivity.java
+++ app/src/main/java/com/version2/swordsandsorcery/MainActivity.java
@@ -1,17 +1,11 @@
 package com.version2.swordsandsorcery;
 
 import android.content.Intent;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/version2/swordsandsorcery/characterCreationOverview.java
+++ app/src/main/java/com/version2/swordsandsorcery/characterCreationOverview.java
@@ -1,33 +1,9 @@
 package com.version2.swordsandsorcery;
 
-import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.util.Log;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.Spinner;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.content.Intent;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 public class characterCreationOverview extends AppCompatActivity {
