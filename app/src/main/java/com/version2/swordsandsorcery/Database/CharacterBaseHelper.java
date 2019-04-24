dd5b42bd00eac2268796ae812fbf38cffcd0179d
Sparky <spenciev3@yahoo.com>
2019-04-23 18:16:25 -0700

    copied over base helper

--- app/src/main/java/com/version2/swordsandsorcery/Database/CharacterBaseHelper.java
+++ app/src/main/java/com/version2/swordsandsorcery/Database/CharacterBaseHelper.java
@@ -3,8 +3,11 @@ package com.version2.swordsandsorcery.Database;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.content.Context;
+import android.nfc.Tag;
+import android.util.Log;
 
 import com.version2.swordsandsorcery.Database.CharacterDB.CharacterTable;
+import static com.version2.swordsandsorcery.Database.CharacterDB.CharacterTable.*;
 
 public class CharacterBaseHelper extends SQLiteOpenHelper {
     private static final int VERSION = 1;
@@ -16,30 +19,33 @@ public class CharacterBaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db){
         db.execSQL("create table " + CharacterTable.TABLE_NAME + "(" + " _id integer primary key autoincrement, "
-                + CharacterTable.Cols.NAME
-                + "," + CharacterTable.Cols.CLASS_NAME
-                + "," + CharacterTable.Cols.RACE
-                + "," + CharacterTable.Cols.BACKGROUND
-                + "," + CharacterTable.Cols.LVL
-                + "," + CharacterTable.Cols.ABILITY_SCORES
-                + "," + CharacterTable.Cols.ITEMS
-                + "," + CharacterTable.Cols.SPELLS
-                + "," + CharacterTable.Cols.FEATS
-                + "," + CharacterTable.Cols.EXP
-                + "," + CharacterTable.Cols.HITPOINTS
-                + "," + CharacterTable.Cols.SKILLS_PROFICIENCIES
-                + "," + CharacterTable.Cols.MONEY
-                + "," + CharacterTable.Cols.ALIGNMENT
-                + "," + CharacterTable.Cols.ARMOR_CLASS
-                + "," + CharacterTable.Cols.SAVING_THROWS
-                + "," + CharacterTable.Cols.LANGUAGES
-                + "," + CharacterTable.Cols.EQUIPMENT
-                + "," + CharacterTable.Cols.SPEED
-                + "," + CharacterTable.Cols.INITIATIVE
-                + "," + CharacterTable.Cols.HIT_DICE + ")");
+                + CharacterTable.CharactersColumns.NAME
+                + "," + CharacterTable.CharactersColumns.CLASS_NAME
+                + "," + CharacterTable.CharactersColumns.RACE
+                + "," + CharacterTable.CharactersColumns.BACKGROUND
+                + "," + CharacterTable.CharactersColumns.LVL
+                + "," + CharacterTable.CharactersColumns.ABILITY_SCORES
+                + "," + CharacterTable.CharactersColumns.ITEMS
+                + "," + CharacterTable.CharactersColumns.SPELLS
+                + "," + CharacterTable.CharactersColumns.FEATS
+                + "," + CharacterTable.CharactersColumns.EXP
+                + "," + CharacterTable.CharactersColumns.HITPOINTS
+                + "," + CharacterTable.CharactersColumns.SKILLS_PROFICIENCIES
+                + "," + CharacterTable.CharactersColumns.MONEY
+                + "," + CharacterTable.CharactersColumns.ALIGNMENT
+                + "," + CharacterTable.CharactersColumns.ARMOR_CLASS
+                + "," + CharacterTable.CharactersColumns.SAVING_THROWS
+                + "," + CharacterTable.CharactersColumns.LANGUAGES
+                + "," + CharacterTable.CharactersColumns.EQUIPMENT
+                + "," + CharacterTable.CharactersColumns.SPEED
+                + "," + CharacterTable.CharactersColumns.INITIATIVE
+                + "," + CharacterTable.CharactersColumns.HIT_DICE + ")");
     }
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
 
+        db.execSQL("Drop Table if Exists " + TABLE_NAME);
+        onCreate(db);
     }
+
 }
